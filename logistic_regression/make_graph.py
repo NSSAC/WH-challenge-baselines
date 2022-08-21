60a94f071d17f2c1f148113399a99c044f4282a2
Przemek Porebski <pjp2b@virginia.edu>
2022-08-20 22:40:42 -0400

    Changed sample data to correspond to one of the partitions

--- logistic_regression/make_graph.py
+++ logistic_regression/make_graph.py
@@ -10,9 +10,12 @@ def make_graph(csvfile):
 
     edges = pd.read_csv(csvfile)
 
-    edges["pid1"] = edges.apply(lambda row: min(row["sourcePID"], row["targetPID"]), axis=1)
-    edges["pid2"] = edges.apply(lambda row: max(row["sourcePID"], row["targetPID"]), axis=1)
+    edges["pid1s"] = edges.apply(lambda row: min(row["pid1"], row["pid2"]), axis=1)
+    edges["pid2s"] = edges.apply(lambda row: max(row["pid1"], row["pid2"]), axis=1)
+    
+    edges = edges.drop(columns=['pid1', 'pid2'])
+    edges = edges.rename(columns={'pid1s':'pid1', 'pid2s':'pid2'})
 
     collapsed_edges = edges[["pid1", "pid2", "duration"]].groupby(["pid1", "pid2"]).sum()
-
+    
     return Graph.DataFrame(collapsed_edges.reset_index(), directed=False)
